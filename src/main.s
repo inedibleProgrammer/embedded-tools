f5a3c32eb5ca8d82058d77822aa7082551367082
inedible programmer <inedibleProgrammer@gmail.com>
2025-05-30 23:24:13 -0400

    Finally it makes sense

--- src/main.s
+++ src/main.s
@@ -2,12 +2,16 @@
 .cpu cortex-m4
 .thumb
 
+.word 0x20020000        /* _estack: top of RAM */
+.word _start+1            /* Reset handler. The +1 is necessary to specify thumb mode */
+.word Default_Handler   /* NMI_Handler */
+.word Default_Handler   /* HardFault_Handler */
 
-.section .text
-.word 0x20020000        /* _estack: Top of 128KB RAM */
-.word _start            /* Reset handler */
-
-
+.text
 .global _start
 _start:
-    b .                 /* Infinite loop */
+    b .                 /* Just infinite loop */
+
+.type Default_Handler, %function
+Default_Handler:
+    b .                 /* Also infinite loop */

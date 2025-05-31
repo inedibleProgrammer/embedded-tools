db0d52a43cd3afb1b7b3748ee35a990269341b69
inedible programmer <inedibleProgrammer@gmail.com>
2025-05-30 23:02:43 -0400

    I learned that .text starts at 0x08000000

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,8 +4,8 @@ echo "Compiling..."
 arm-none-eabi-as -g -mcpu=cortex-m4 -mthumb src/main.s -o build/main.o
 
 echo "Linking..."
-# arm-none-eabi-ld -g -Ttext=0x08000000 build/main.o -o build/main.elf
-arm-none-eabi-ld -g build/main.o -o build/main.elf
+arm-none-eabi-ld -g -Ttext=0x08000000 build/main.o -o build/main.elf
+# arm-none-eabi-ld -g -Ttext=0 build/main.o -o build/main.elf
 
 echo "Downloading..."
 openocd -f interface/stlink.cfg -f target/stm32f4x.cfg -c "program build/main.elf reset exit"
--- src/main.s
+++ src/main.s
@@ -2,54 +2,12 @@
 .cpu cortex-m4
 .thumb
 
-// Vector table section placed at beginning of flash
-.section .isr_vector, "a", %progbits
-.type g_pfnVectors, %object
-.size g_pfnVectors, .-g_pfnVectors
 
-g_pfnVectors:
-    .word _estack             // Initial stack pointer
-    .word Reset_Handler       // Reset vector
+.section .text
+.word 0x20020000        /* _estack: Top of 128KB RAM */
+.word _start            /* Reset handler */
 
-// Top of stack: end of SRAM (STM32F401CC = 0x20018000)
-.equ _estack, 0x20018000
 
-.equ RCC_AHB1ENR,    0x40023830
-.equ GPIOA_MODER,    0x40020000
-.equ GPIOA_ODR,      0x40020014
-
-// Define the Reset Handler (entry point)
-.section .text.Reset_Handler, "ax", %progbits
-.global Reset_Handler
-.type Reset_Handler, %function
-
-Reset_Handler:
-    b _start                  // Branch to _start (optional indirection)
-
-// Define _start for symbolic/debugging use
-.section .text._start, "ax", %progbits
 .global _start
-.type _start, %function
-
 _start:
-    /* Enable clock to GPIOA (bit 0 in RCC_AHB1ENR) */
-    LDR R0, =RCC_AHB1ENR
-    LDR R1, [R0]
-    ORR R1, R1, #(1 << 0)
-    STR R1, [R0]
-
-    ;; /* Set PA5 (bit 10/11 in GPIOA_MODER) to output (01) */
-    ;; LDR R0, =GPIOA_MODER
-    ;; LDR R1, [R0]
-    ;; BIC R1, R1, #(0b11 << (5 * 2))  /* Clear bits 11:10 */
-    ;; ORR R1, R1, #(0b01 << (5 * 2))  /* Set bits 11:10 to 01 */
-    ;; STR R1, [R0]
-
-    ;; /* Set PA5 high (bit 5 in GPIOA_ODR) */
-    ;; LDR R0, =GPIOA_ODR
-    ;; LDR R1, [R0]
-    ;; ORR R1, R1, #(1 << 5)
-    ;; STR R1, [R0]
-
-loop:
-    B loop    /* Infinite loop */
+    b .                 /* Infinite loop */

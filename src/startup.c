29426d31272bbd051a6f8202991431e0d3fa4e1e
inedible programmer <inedibleProgrammer@gmail.com>
2025-05-31 10:15:02 -0400

    Add more interrupt handlers

--- src/startup.c
+++ src/startup.c
@@ -12,14 +12,58 @@ int main(void);
 
 // Minimal weak handler declarations
 void Reset_Handler(void);
-void Default_Handler(void) { while (1); }
+void NMI_Handler(void)                __attribute__((weak, alias("Default_Handler")));
+void HardFault_Handler(void)         __attribute__((weak, alias("Default_Handler")));
+void MemManage_Handler(void)         __attribute__((weak, alias("Default_Handler")));
+void BusFault_Handler(void)          __attribute__((weak, alias("Default_Handler")));
+void UsageFault_Handler(void)        __attribute__((weak, alias("Default_Handler")));
+void SVC_Handler(void)               __attribute__((weak, alias("Default_Handler")));
+void DebugMon_Handler(void)          __attribute__((weak, alias("Default_Handler")));
+void PendSV_Handler(void)            __attribute__((weak, alias("Default_Handler")));
+void SysTick_Handler(void)           __attribute__((weak, alias("Default_Handler")));
+
+// STM32F401RET6 specific interrupt handlers
+void WWDG_IRQHandler(void)           __attribute__((weak, alias("Default_Handler")));
+void PVD_IRQHandler(void)            __attribute__((weak, alias("Default_Handler")));
+void TAMP_STAMP_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void RTC_WKUP_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void FLASH_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+void RCC_IRQHandler(void)            __attribute__((weak, alias("Default_Handler")));
+void EXTI0_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+void EXTI1_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+
+// Default handler
+void Default_Handler(void) {
+    while (1);
+}
 
 // Vector table
 __attribute__((section(".isr_vector")))
-void (* const vector_table[])(void) = {
-    (void (*)(void))(&_estack), // Initial stack pointer
-    Reset_Handler,              // Reset handler
-    // Other IRQ handlers can go here...
+void (* const vector_table[])(void) =
+{
+    (void (*)(void))(&_estack), // Initial stack pointer. Cast to function pointer type to be consistent with vector table.
+    Reset_Handler,                  // Reset Handler
+    NMI_Handler,                    // NMI Handler
+    HardFault_Handler,              // Hard Fault Handler
+    MemManage_Handler,              // MPU Fault Handler
+    BusFault_Handler,               // Bus Fault Handler
+    UsageFault_Handler,             // Usage Fault Handler
+    0, 0, 0, 0,                     // Reserved
+    SVC_Handler,                    // SVCall Handler
+    DebugMon_Handler,               // Debug Monitor Handler
+    0,                              // Reserved
+    PendSV_Handler,                 // PendSV Handler
+    SysTick_Handler,                // SysTick Handler
+
+    // External Interrupts (IRQs)
+    WWDG_IRQHandler,                // Window Watchdog
+    PVD_IRQHandler,                 // PVD through EXTI Line detect
+    TAMP_STAMP_IRQHandler,          // Tamper and TimeStamp
+    RTC_WKUP_IRQHandler,            // RTC Wakeup
+    FLASH_IRQHandler,               // FLASH
+    RCC_IRQHandler,                 // RCC
+    EXTI0_IRQHandler,               // EXTI Line0
+    EXTI1_IRQHandler,               // EXTI Line1
 };
 
 // Reset handler
